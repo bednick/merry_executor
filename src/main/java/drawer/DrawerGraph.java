a7fadc85cedda52d9bd46188654de6890daa3179
Anastasia <prokopyeva96@yandex.ru>
2017-12-18 01:42:57 +0700

    added new class

---
+++ src/main/java/drawer/DrawerGraph.java
@@ -0,0 +1,56 @@
+package drawer;
+
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.event.GraphEvent;
+import edu.uci.ics.jung.visualization.VisualizationImageServer;
+import edu.uci.ics.jung.visualization.decorators.EdgeShape;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import sun.tools.tree.Node;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Anastasia on 17.12.2017.
+ */
+public class DrawerGraph implements IDrawer {
+
+
+    DrawerGraph(DirectedSparseGraph graph){
+
+    }
+
+    @Override
+    public void draw(Object graph) {
+        DirectedSparseGraph g = (DirectedSparseGraph) graph;
+        g.addVertex("Vertex1");
+        g.addVertex("Vertex2");
+        g.addVertex("Vertex3");
+        g.addEdge("Edge1", "Vertex1", "Vertex2");
+        g.addEdge("Edge2", "Vertex1", "Vertex3");
+        g.addEdge("Edge3", "Vertex3", "Vertex1");
+
+
+
+        VisualizationImageServer vs =
+                new VisualizationImageServer(
+                        new CircleLayout(g), new Dimension(400, 400));
+
+        vs.setBackground(Color.WHITE);
+        vs.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller<GraphEvent.Edge>());
+        vs.getRenderContext().setEdgeShapeTransformer(new EdgeShape.Line<Node, GraphEvent.Edge>());
+        vs.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<Node>());
+        vs.getColorModel();
+//        vs.getRenderer().getVertexLabelRenderer()
+//                .setPosition(Renderer.VertexLabel.Position.CNTR);
+
+
+
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(vs);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
--- src/main/java/drawer/IDrawer.java
+++ src/main/java/drawer/IDrawer.java
@@ -15,41 +15,13 @@ import java.io.File;
 import java.util.Properties;
 
 public interface IDrawer {
-    void draw(Object graph);
+    void draw(Object graph); //в каком виде мне будет даваться граф????
 
     public static void main(String [] args) {
         DirectedSparseGraph g = new DirectedSparseGraph();
-        g.addVertex("Vertex1");
-        g.addVertex("Vertex2");
-        g.addVertex("Vertex3");
-        g.addVertex(2);
-        g.addEdge("Edge1", "Vertex1", "Vertex2");
-        g.addEdge("Edge2", "Vertex1", "Vertex3");
-        g.addEdge("Edge3", "Vertex3", "Vertex1");
-
-
-
-        VisualizationImageServer vs =
-                new VisualizationImageServer(
-                        new CircleLayout(g), new Dimension(200, 200));
-
-        vs.setBackground(Color.WHITE);
-        vs.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller<GraphEvent.Edge>());
-        vs.getRenderContext().setEdgeShapeTransformer(new EdgeShape.Line<Node, GraphEvent.Edge>());
-        vs.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<Node>());
-        vs.getColorModel();
-//        vs.getRenderer().getVertexLabelRenderer()
-//                .setPosition(Renderer.VertexLabel.Position.CNTR);
-
-
-
-        JFrame frame = new JFrame();
-        frame.getContentPane().add(vs);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
         
 
     }
 
 }
+
