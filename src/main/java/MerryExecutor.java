ee3fe2b0fb4ea987a5028faa92da532affb3a6ed
nikkollaii <79139978241@mail.ru>
2017-12-21 01:06:15 +0700

    update path

--- src/main/java/MerryExecutor.java
+++ src/main/java/MerryExecutor.java
@@ -1,7 +1,24 @@
 
+import bricks.Command;
+import executor.IExecutor;
+import executor.ISorter;
+import parser.IParser;
+import parser.Parser;
+
+import java.io.IOException;
+import java.util.Collection;
 
 public class MerryExecutor {
     public static void main(String[] args) {
+        IParser parser = new Parser();
+        ISorter sorter = null;
+        IExecutor executor = null;
+        try {
+            parser.load(args[0]);
+            Collection<Command> commands = parser.getData();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
