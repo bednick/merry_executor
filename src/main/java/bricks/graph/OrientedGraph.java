40dd6da8bae8c93f5ca4c5b4bea45d1a332036ce
nikkollaii <79139978241@mail.ru>
2017-12-23 02:00:53 +0700

    add OrientedGraph

---
+++ src/main/java/bricks/graph/OrientedGraph.java
@@ -0,0 +1,50 @@
+package bricks.graph;
+
+import bricks.Pair;
+
+import java.util.*;
+
+/**
+ *
+ */
+public class OrientedGraph<T> {
+    private Map<T, Vertex<T>> vertices;
+    private Set<Vertex<T>> root;
+    private Set<Vertex<T>> leaf;
+
+    public OrientedGraph() {
+        vertices = new HashMap<>();
+        root = new HashSet<>();
+        leaf = new HashSet<>();
+    }
+
+    public void addVertex(T vertex) {
+        Vertex<T> v = new Vertex<T>(vertex);
+        vertices.put(vertex, v);
+        root.add(v);
+        leaf.add(v);
+    }
+
+    public void addEdge(Pair<T, T> edge) {
+        Vertex<T> v1 = vertices.get(edge.getKey());
+        Vertex<T> v2 = vertices.get(edge.getValue());
+
+        leaf.remove(v1);
+        root.remove(v2);
+
+        v1.addOut(v2);
+        v2.addIn(v1);
+    }
+
+    public Set<Vertex<T>> getRoot() {
+        return root;
+    }
+
+    public Set<Vertex<T>> getLeaf() {
+        return leaf;
+    }
+
+    public Vertex<T> getVertex(T object) {
+        return vertices.get(object);
+    }
+}
