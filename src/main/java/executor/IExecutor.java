c1e6c93a821dc35667c0fc7bac27cd5c24a41907
nikkollaii <79139978241@mail.ru>
2017-12-23 04:48:37 +0700

    change interface IExecutor

--- src/main/java/executor/IExecutor.java
+++ src/main/java/executor/IExecutor.java
@@ -1,9 +1,7 @@
 package executor;
 
-import bricks.Command;
-
 import java.util.List;
 
 public interface IExecutor {
-    void exec(List<Command> commands);
+    void exec(List<String> commands);
 }
