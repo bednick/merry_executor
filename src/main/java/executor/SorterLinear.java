8965161861d36c0ea756017194161357c85ec304
Bedarev N.A <79139978241@mail.ru>
2017-12-22 19:01:28 +0700

    init linear sorter

---
+++ src/main/java/executor/SorterLinear.java
@@ -0,0 +1,14 @@
+package executor;
+
+import bricks.Command;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SorterLinear implements ISorter{
+
+    @Override
+    public List<Command> sort(Collection<Command> data, List<String> outFiles) {
+        return null;
+    }
+}
