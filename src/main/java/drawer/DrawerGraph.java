05ecd67f89219f1b1a1f2167ca71416d7b8a6293
Anastasia <prokopyeva96@yandex.ru>
2017-12-23 02:52:06 +0700

    fixed drawer

--- src/main/java/drawer/DrawerGraph.java
+++ src/main/java/drawer/DrawerGraph.java
@@ -30,13 +30,6 @@ public class DrawerGraph implements IDrawer {
         parse(graph);
         g = new DirectedSparseGraph();
 
-//        g.addVertex("Vertex1");
-//        g.addVertex("Vertex2");
-//        g.addVertex("Vertex3");
-//        g.addEdge("Edge1", "Vertex1", "Vertex2");
-//        g.addEdge("Edge2", "Vertex1", "Vertex3");
-//        g.addEdge("Edge3", "Vertex3", "Vertex1");
-
         VisualizationImageServer vs =
                 new VisualizationImageServer(
                         new CircleLayout(g), new Dimension(400, 400));
