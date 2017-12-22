12f9c6aee0d318d81b3ca7fa6ee149b6676506fe
AnastasiaProk <prokopyeva96@yandex.ru>
2017-12-23 03:22:10 +0700

    Update DrawerGraph.java

--- src/main/java/drawer/DrawerGraph.java
+++ src/main/java/drawer/DrawerGraph.java
@@ -19,30 +19,6 @@ public class DrawerGraph implements IDrawer {
 
     @Override
     public void draw(Collection<Command> graph) {
-//        DirectedSparseGraph g = (DirectedSparseGraph) graph;
-//        g.addVertex("Vertex1");
-//        g.addVertex("Vertex2");
-//        g.addVertex("Vertex3");
-//        g.addEdge("Edge1", "Vertex1", "Vertex2");
-//        g.addEdge("Edge2", "Vertex1", "Vertex3");
-//        g.addEdge("Edge3", "Vertex3", "Vertex1");
-//
-//        VisualizationImageServer vs =
-//                new VisualizationImageServer(
-//                        new CircleLayout(g), new Dimension(400, 400));
-//
-//        vs.setBackground(Color.WHITE);
-//        vs.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller<GraphEvent.Edge>());
-//        vs.getRenderContext().setEdgeShapeTransformer(new EdgeShape.Line<Node, GraphEvent.Edge>());
-//        vs.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<Node>());
-//        vs.getColorModel();
-////        vs.getRenderer().getVertexLabelRenderer()
-////                .setPosition(Renderer.VertexLabel.Position.CNTR);
-//
-//        JFrame frame = new JFrame();
-//        frame.getContentPane().add(vs);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.pack();
-//        frame.setVisible(true);
+
     }
 }
