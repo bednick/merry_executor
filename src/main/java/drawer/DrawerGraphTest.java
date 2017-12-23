7cc90d78b748fb427c420c167704f846eaba40a0
Anastasia <prokopyeva96@yandex.ru>
2017-12-23 13:40:52 +0700

    add test

---
+++ src/main/java/drawer/DrawerGraphTest.java
@@ -0,0 +1,14 @@
+package drawer;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Anastasia on 23.12.2017.
+ */
+public class DrawerGraphTest {
+    @org.junit.Test
+    public void draw() throws Exception {
+        assertEquals(1, 1);
+    }
+
+}
