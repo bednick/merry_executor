fafa7e0e075985002f7bbe0b2d78fd5823cf6fc2
TheBestUser <a.kuzmichev@g.nsu.ru>
2017-12-23 13:33:33 +0700

    Added constructor for exception

--- src/main/java/executor/FailProcessingException.java
+++ src/main/java/executor/FailProcessingException.java
@@ -2,5 +2,13 @@ package executor;
 
 public class FailProcessingException extends Exception
 {
-
+	public FailProcessingException()
+	{
+		super();
+	}
+	
+	public FailProcessingException(String mes)
+	{
+		super(mes);
+	}
 }
